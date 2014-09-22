ca439e38329870d01973aaaa668b3ee4f6c0f42f
Hanibalus <eugen_9998@yahoo.com>
2014-09-22 11:21:16 +0300

    Update SingleObject.java

--- src/designpattern/singletonPattern/SingleObject.java
+++ src/designpattern/singletonPattern/SingleObject.java
@@ -7,6 +7,7 @@ package designpattern.singletonPattern;
  */
 public class SingleObject {
 //create an object of SingleObject
+   int i=0;
    private static final SingleObject instance = new SingleObject();
 
    //make the constructor private so that this class cannot be
